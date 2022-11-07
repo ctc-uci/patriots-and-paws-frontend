9879badfbc3e372f1bb6922c72ac0629cf4ec737
Alan Chang <alan@alanchang.xyz>
2022-11-07 02:14:08 -0800

    Check-in package-lock/common axios instance utils, change default readme to use npm instead of yarn

---
+++ src/util/utils.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+let baseURL = '';
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  // dev code
+  baseURL = `${process.env.REACT_APP_API_URL}`;
+} else {
+  // production code
+  baseURL = `${process.env.REACT_APP_PROD_API_URL}`;
+}
+
+// See auth_utils for AuthInterceptor
+const PNPBackend = axios.create({
+  baseURL,
+});
+
+// eslint-disable-next-line import/prefer-default-export
+export { PNPBackend };
