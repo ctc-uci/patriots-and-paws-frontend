6250ee2532c641d97655e805b143ae36c5c5a832
jrock-3 <justin.liao3@gmail.com>
2022-11-08 14:25:29 -0800

    Install ChakraUI, Remove CRA Defaults

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,21 @@
+import { ChakraProvider, Button, Stack } from '@chakra-ui/react';
+import { PhoneIcon } from '@chakra-ui/icons';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Stack spacing={4} direction="column" align="center">
+        <PhoneIcon />
+        <Button colorScheme="teal" size="lg">
+          Test
+        </Button>
+        <Button colorScheme="teal" size="lg">
+          Button
+        </Button>
+      </Stack>
+    </ChakraProvider>
   );
 }
 
