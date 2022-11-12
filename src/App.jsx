17226d8c291c02033aafb65c7e8cabaacdae3c24
Rochelle Nixon <rochelln@uci.edu>
2022-11-12 15:30:51 -0800

    added dropzone component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-import { ChakraProvider, Button, Stack } from '@chakra-ui/react';
-import { PhoneIcon } from '@chakra-ui/icons';
-import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import React, { useState } from 'react';
 import './App.css';
+import DropZone from './components/DropZone/DropZone';
 
 function App() {
+  const [files, setFiles] = useState([]);
+
   return (
     <ChakraProvider>
-      <Stack spacing={4} direction="column" align="center">
-        <PhoneIcon />
-        <Button colorScheme="teal" size="lg">
-          Test
-        </Button>
-        <Button colorScheme="teal" size="lg">
-          Button
-        </Button>
-      </Stack>
+      <DropZone setFiles={setFiles} />
+      {files}
     </ChakraProvider>
   );
 }
