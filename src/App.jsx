9ec357fc6602fd8e96d69ee4b01bfccf55a990ee
Caroline Nguyen <carolbnguyen@gmail.com>
2022-11-15 22:04:15 -0800

    Set up routes in App.jsx and add ProtectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,51 @@
-import { ChakraProvider, Button, Stack } from '@chakra-ui/react';
-import { PhoneIcon } from '@chakra-ui/icons';
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+import ProtectedRoute from './utils/ProtectedRoute';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
+import Logout from './components/Logout/Logout';
+import ForgotPassword from './components/ForgotPassword/ForgotPassword';
+import Dashboard from './components/Dashboard/Dashboard';
+
+import AUTH_ROLES from './utils/AuthConfig';
+
+const { SUPERADMIN_ROLE, ADMIN_ROLE, DRIVER_ROLE } = AUTH_ROLES.AUTH_ROLES;
 
 function App() {
   return (
     <ChakraProvider>
-      <Stack spacing={4} direction="column" align="center">
-        <PhoneIcon />
-        <Button colorScheme="teal" size="lg">
-          Test
-        </Button>
-        <Button colorScheme="teal" size="lg">
-          Button
-        </Button>
-      </Stack>
+      <Router>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <ProtectedRoute
+                Component={<Dashboard />}
+                redirectPath="/login"
+                roles={[SUPERADMIN_ROLE, ADMIN_ROLE]}
+              />
+            }
+          />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route
+            exact
+            path="/logout"
+            element={
+              <ProtectedRoute
+                Component={<Logout />}
+                redirectPath="/login"
+                roles={[SUPERADMIN_ROLE, ADMIN_ROLE, DRIVER_ROLE]}
+              />
+            }
+          />
+          <Route exact path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
     </ChakraProvider>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
-// import App from './App';
-
-import Register from './components/Register/Register';
-import LogIn from './components/LogIn/LogIn';
-import LogOut from './components/LogOut/LogOut';
-import ForgotPassword from './components/ForgotPassword/ForgotPassword';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Router>
-      <Routes>
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/login" element={<LogIn />} />
-        <Route exact path="/logout" element={<LogOut />} />
-        <Route exact path="/forgot-password" element={<ForgotPassword />} />
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
