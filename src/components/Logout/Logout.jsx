ad57b91d549558e141707ac2f10297176b6c84e8
Caroline Nguyen <carolbnguyen@gmail.com>
2022-11-29 20:25:50 -0800

    Add yup package for form validation

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { instanceOf } from 'prop-types';
 import { Flex, Button, Heading, Stack, Box } from '@chakra-ui/react';
 import { logout, useNavigate } from '../../utils/AuthUtils';
-import { Cookies, withCookies, clearCookies } from '../../utils/CookieUtils';
+import { Cookies, withCookies } from '../../utils/CookieUtils';
 import styles from './Logout.module.css';
 
 const Logout = ({ cookies }) => {
@@ -10,7 +10,6 @@ const Logout = ({ cookies }) => {
   const [errorMessage, setErrorMessage] = useState();
   const handleSubmit = async () => {
     try {
-      clearCookies(cookies);
       await logout('/login', navigate, cookies);
     } catch (err) {
       setErrorMessage(err.message);
@@ -22,13 +21,7 @@ const Logout = ({ cookies }) => {
       <Stack align="center">
         <Heading>Logout</Heading>
         {errorMessage && <Box>{errorMessage}</Box>}
-        <Button
-          colorScheme="blue"
-          className={styles['logout-button']}
-          onClick={e => {
-            handleSubmit(e);
-          }}
-        >
+        <Button colorScheme="blue" className={styles['logout-button']} onSubmit={handleSubmit}>
           Logout
         </Button>
       </Stack>
