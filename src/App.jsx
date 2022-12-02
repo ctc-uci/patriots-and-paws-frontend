8ee43482c21067bc1f7f160ea2b72882f7e12a05
Rayan Tighiouart <t.alex.2002@gmail.com>
2022-12-02 10:56:56 -0800

    rt - deleted redundant donationform lines,imports,useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { ChakraProvider, Button, Image } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import DropZone from './components/DropZone/DropZone';
-import uploadImage from './util/furnitureUtils';
 import DonationForm from './components/DonationForm/DonationForm';
 
 import Dashboard from './pages/dashboard/Dashboard';
@@ -13,23 +11,9 @@ import DriverRoutes from './pages/dashboard/DriverRoutes';
 import DonateStatus from './pages/donation/DonateStatus';
 
 function App() {
-  const [files, setFiles] = useState([]);
-  const [images, setImages] = useState([]);
-
-  const onSubmit = async () => {
-    const url = await uploadImage(files[0]);
-    setImages(prev => [...prev, url]);
-  };
-
   return (
     <Router>
       <ChakraProvider>
-        <DropZone setFiles={setFiles} />
-        <Button onClick={onSubmit}>Upload File</Button>
-        {images.map(e => (
-          <Image key={e} src={e} />
-        ))}
-        <DonationForm />
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/donate/edit" element={<EditDonationForm />} />
