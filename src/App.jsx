f3d090b9a4609ae63cc8c495ed6ad363fed6bced
ctc-devops <90984711+ctc-devops@users.noreply.github.com>
2022-12-04 16:31:31 -0800

    Set Up Frontend Routes (#20)
    * created frontend routes
    
    * Create a pull trequest for branch 14-setup-frontend-routes
    
    * created frontend routes
    
    * Delete Donate.jsx
    
    * Resolved readability issues
    
    * Resolved readability issues
    
    * Delete DonateEdit.jsx
    
    * Delete Routes.jsx
    
    * Update DriverRoutes.jsx
    
    * rt - changed props to url params
    
    * rt - commit yarn.lock, package.json
    
    * rt - deleted redundant donationform lines,imports,useeffect
    
    * rt - readded image/button/dropzone
    
    Co-authored-by: Rayan Tighiouart <t.alex.2002@gmail.com>
    Co-authored-by: Rachel <ravillamor23@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { ChakraProvider, Button, Image } from '@chakra-ui/react';
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import DonationForm from './components/DonationForm/DonationForm';
 import DropZone from './components/DropZone/DropZone';
 import uploadImage from './util/furnitureUtils';
-import DonationForm from './components/DonationForm/DonationForm';
+import Dashboard from './pages/dashboard/Dashboard';
+import EditDonationForm from './pages/dashboard/EditDonationForm';
+import Drivers from './pages/dashboard/Drivers';
+import DriverRoutes from './pages/dashboard/DriverRoutes';
+import DonateStatus from './pages/donation/DonateStatus';
 
 function App() {
   const [files, setFiles] = useState([]);
@@ -15,14 +21,23 @@ function App() {
   };
 
   return (
-    <ChakraProvider>
-      <DropZone setFiles={setFiles} />
-      <Button onClick={onSubmit}>Upload File</Button>
-      {images.map(e => (
-        <Image key={e} src={e} />
-      ))}
-      <DonationForm />
-    </ChakraProvider>
+    <Router>
+      <ChakraProvider>
+        <DropZone setFiles={setFiles} />
+        <Button onClick={onSubmit}>Upload File</Button>
+        {images.map(e => (
+          <Image key={e} src={e} />
+        ))}
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/donate/edit" element={<EditDonationForm />} />
+          <Route path="/drivers:id" element={<Drivers />} />
+          <Route path="/driver-routes:id" element={<DriverRoutes />} />
+          <Route path="/donate" element={<DonationForm />} />
+          <Route path="/donate/status" element={<DonateStatus />} />
+        </Routes>
+      </ChakraProvider>
+    </Router>
   );
 }
 
