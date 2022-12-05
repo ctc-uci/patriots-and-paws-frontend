ac7f92aeff7bcc04eab687a3c725ac0d3335548b
Rayan Tighiouart <t.alex.2002@gmail.com>
2022-12-04 16:25:22 -0800

    rt - readded image/button/dropzone

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import React from 'react';
+import { ChakraProvider, Button, Image } from '@chakra-ui/react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import DonationForm from './components/DonationForm/DonationForm';
-
+import DropZone from './components/DropZone/DropZone';
+import uploadImage from './util/furnitureUtils';
 import Dashboard from './pages/dashboard/Dashboard';
 import EditDonationForm from './pages/dashboard/EditDonationForm';
 import Drivers from './pages/dashboard/Drivers';
@@ -11,9 +12,22 @@ import DriverRoutes from './pages/dashboard/DriverRoutes';
 import DonateStatus from './pages/donation/DonateStatus';
 
 function App() {
+  const [files, setFiles] = useState([]);
+  const [images, setImages] = useState([]);
+
+  const onSubmit = async () => {
+    const url = await uploadImage(files[0]);
+    setImages(prev => [...prev, url]);
+  };
+
   return (
     <Router>
       <ChakraProvider>
+        <DropZone setFiles={setFiles} />
+        <Button onClick={onSubmit}>Upload File</Button>
+        {images.map(e => (
+          <Image key={e} src={e} />
+        ))}
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/donate/edit" element={<EditDonationForm />} />
