4f1d4fef67eb8738eef4c369ae99a80bbfc11dd7
Hang Cao <hmcao1@uci.edu>
2022-12-13 01:30:26 -0800

    update dashboard content

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Flex, Heading, Stack, Text } from '@chakra-ui/react';
+import Logout from '../../components/Logout/Logout';
+import { getUserFromDB } from '../../utils/AuthUtils';
 
 const Dashboard = () => {
-  return <p>Dashboard Page</p>;
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const fetchUserFromDB = async () => {
+      const userFromDB = await getUserFromDB();
+      setUser(userFromDB);
+    };
+    fetchUserFromDB();
+  }, []);
+
+  return (
+    <Flex minH="100vh" align="center" justify="center">
+      <Stack align="center">
+        <Heading>DASHBOARD</Heading>
+        <Text>Hello, {user.firstName}!</Text>
+        <Logout />
+      </Stack>
+    </Flex>
+  );
 };
 
 export default Dashboard;
