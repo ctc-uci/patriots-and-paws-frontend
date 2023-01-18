079ffc1ba8832f5d51154c0c2c377ec0f6643faa
Rochelle Nixon <rochelln@uci.edu>
2023-01-17 21:04:16 -0800

    created table, search, tag, and add staff button

--- src/pages/ManageStaff/ManageStaff.jsx
+++ src/pages/ManageStaff/ManageStaff.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import {
-  ListItem,
-  List,
   Flex,
   Text,
   Image,
@@ -11,65 +9,124 @@ import {
   ModalOverlay,
   ModalContent,
   ModalCloseButton,
-  LinkBox,
-  LinkOverlay,
+  TableContainer,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Td,
+  Tbody,
+  Tag,
+  InputGroup,
+  InputLeftElement,
+  Input,
+  TagCloseButton,
 } from '@chakra-ui/react';
-import { StarIcon, HamburgerIcon } from '@chakra-ui/icons';
+import {
+  DeleteIcon,
+  EditIcon,
+  EmailIcon,
+  PhoneIcon,
+  SearchIcon,
+  SmallAddIcon,
+} from '@chakra-ui/icons';
 import styles from './ManageStaff.css';
 import CreateAccount from '../../components/CreateAccount/CreateAccount';
-import profilePicture from '../../assets/profilePicture.png';
-import DisplayedProfile from '../../components/DisplayedProfile/DisplayedProfile';
+import cardAccount from '../../assets/card-account-details.png';
+import nameIcon from '../../assets/Bold.png';
+import menuIcon from '../../assets/Sort.png';
 
 const ManageStaff = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const firstNames = ['Manny', 'John', 'Mary', 'Lisa'];
-  const lastNames = ['Jacinto', 'Paul', 'Ann', 'Walter'];
+  const names = ['Manny Jacinto', 'John Paul', 'Mary Ann', 'Lisa Walter'];
+  const emails = [
+    'ncarson@gmail.com',
+    'ncarson@gmail.com',
+    'ncarson@gmail.com',
+    'ncarson@gmail.com',
+  ];
+  const phones = ['818-293-9998', '818-293-9998', '818-293-9998', '818-293-9998'];
   const roles = ['Driver', 'Admin', 'Driver', 'Admin'];
 
   return (
-    <Flex>
-      <Flex className="side-panel">
-        <Flex className={styles['panel-title']} align="center">
-          <StarIcon />
-          <Text className="staff-title">Staff</Text>
-          <HamburgerIcon ml={250} />
-        </Flex>
-        <Flex minH="100vh">
-          <List className="users">
-            {firstNames.map((name, index) => (
-              <LinkBox key={name}>
-                <LinkOverlay href="#">
-                  <ListItem key={name} mb={0} mt={0} border="1px" borderColor="gray.200">
-                    <Flex align="center" height="12vh" mr={20} ml={10}>
-                      <Image ml={5} boxSize="50px" src={profilePicture} mr="4vh" />
-                      <Flex className="driver-info">
-                        <Text className="driver-name">
-                          {name} {lastNames[index]}
-                        </Text>
-                        <Text className="role">{roles[index]}</Text>
-                      </Flex>
-                    </Flex>
-                  </ListItem>
-                </LinkOverlay>
-              </LinkBox>
+    <Flex className="column" direction="column">
+      <Flex className="row">
+        <InputGroup>
+          <InputLeftElement pointerEvents="none">
+            <SearchIcon color="gray.300" />
+          </InputLeftElement>
+          <Input placeholder="Search Staff" style={{ width: '500px' }} />
+        </InputGroup>
+        <Tag size="sm" variant="solid" colorScheme="blue">
+          Admin
+          <TagCloseButton />
+        </Tag>
+        <Image src={menuIcon} style={{ width: '20px', height: '20px' }} />
+        <Button colorScheme="blue" className={styles['create-account-button']} onClick={onOpen}>
+          Add Staff <SmallAddIcon />
+        </Button>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalCloseButton />
+            <CreateAccount />
+          </ModalContent>
+        </Modal>
+      </Flex>
+      <TableContainer>
+        <Table variant="striped" colorScheme="gray">
+          <Thead>
+            <Tr>
+              <Th>
+                <Flex>
+                  <Image src={nameIcon} mr={2} />
+                  <Text>Name</Text>
+                </Flex>
+              </Th>
+              <Th align="center">
+                <EmailIcon mr={2} />
+                Email Address
+              </Th>
+              <Th>
+                <PhoneIcon mr={2} />
+                Phone Number
+              </Th>
+              <Th>
+                <Flex>
+                  <Image src={cardAccount} mr={2} />
+                  Role
+                </Flex>
+              </Th>
+              <Th />
+            </Tr>
+          </Thead>
+          <Tbody>
+            {names.map((name, index) => (
+              <Tr key="{name}">
+                <Td>{name}</Td>
+                <Td>{emails[index]}</Td>
+                <Td>{phones[index]}</Td>
+                <Td>
+                  {roles[index] === 'Admin' ? (
+                    <Tag size="sm" variant="solid" colorScheme="blue">
+                      Admin
+                    </Tag>
+                  ) : (
+                    <Tag size="sm" variant="solid" colorScheme="red">
+                      Driver
+                    </Tag>
+                  )}
+                </Td>
+                <Td>
+                  <EditIcon mr={5} />
+                  <DeleteIcon />
+                </Td>
+              </Tr>
             ))}
-            <Button colorScheme="blue" className={styles['create-account-button']} onClick={onOpen}>
-              Create User
-            </Button>
-          </List>
-          <Modal isOpen={isOpen} onClose={onClose}>
-            <ModalOverlay />
-            <ModalContent>
-              <ModalCloseButton />
-              <CreateAccount />
-            </ModalContent>
-          </Modal>
-        </Flex>
-      </Flex>
-      <Flex m="auto">
-        <DisplayedProfile />
-      </Flex>
+          </Tbody>
+        </Table>
+      </TableContainer>
     </Flex>
   );
 };
