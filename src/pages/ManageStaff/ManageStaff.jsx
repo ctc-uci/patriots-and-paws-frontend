55172f06b36be9dbd2fba8798abeed63b1015fab
Rochelle Nixon <rochelln@uci.edu>
2023-01-18 22:05:39 -0800

    formatting page, colors, svg icons

--- src/pages/ManageStaff/ManageStaff.jsx
+++ src/pages/ManageStaff/ManageStaff.jsx
@@ -32,9 +32,9 @@ import {
 } from '@chakra-ui/icons';
 import styles from './ManageStaff.css';
 import CreateAccount from '../../components/CreateAccount/CreateAccount';
-import cardAccount from '../../assets/card-account-details.png';
-import nameIcon from '../../assets/Bold.png';
-import menuIcon from '../../assets/Sort.png';
+import cardAccount from '../../assets/card-account-details.svg';
+import peopleIcon from '../../assets/Bold.svg';
+import menuIcon from '../../assets/Menu.svg';
 
 const ManageStaff = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -50,58 +50,70 @@ const ManageStaff = () => {
   const roles = ['Driver', 'Admin', 'Driver', 'Admin'];
 
   return (
-    <Flex className="column" direction="column">
-      <Flex className="row">
-        <InputGroup>
-          <InputLeftElement pointerEvents="none">
-            <SearchIcon color="gray.300" />
-          </InputLeftElement>
-          <Input placeholder="Search Staff" style={{ width: '500px' }} />
-        </InputGroup>
-        <Tag size="sm" variant="solid" colorScheme="blue">
-          Admin
-          <TagCloseButton />
-        </Tag>
-        <Image src={menuIcon} style={{ width: '20px', height: '20px' }} />
-        <Button colorScheme="blue" className={styles['create-account-button']} onClick={onOpen}>
-          Add Staff <SmallAddIcon />
-        </Button>
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalCloseButton />
-            <CreateAccount />
-          </ModalContent>
-        </Modal>
+    <Flex className="column" direction="column" m={10}>
+      <Flex mb={10} justify="space-between">
+        <Flex>
+          <InputGroup mr={5} width={200}>
+            <InputLeftElement pointerEvents="none">
+              <SearchIcon color="gray.300" />
+            </InputLeftElement>
+            <Input placeholder="Search Staff" style={{ width: '200px' }} />
+          </InputGroup>
+          <Tag variant="solid" colorScheme="blue" font="Inter" size="sm" fontSize={18} ml={0}>
+            Admin
+            <TagCloseButton />
+          </Tag>
+        </Flex>
+        <Flex>
+          <Image
+            src={menuIcon}
+            style={({ width: '40%' }, { height: '40%' }, { float: 'right' })}
+            mr={5}
+          />
+          <Button colorScheme="blue" className={styles['create-account-button']} onClick={onOpen}>
+            Add Staff <SmallAddIcon />
+          </Button>
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalCloseButton />
+              <CreateAccount />
+            </ModalContent>
+          </Modal>
+        </Flex>
       </Flex>
-      <TableContainer>
+      <TableContainer border="1px" borderColor="gray.200">
         <Table variant="striped" colorScheme="gray">
-          <Thead>
-            <Tr>
+          <Thead bgColor="rgb(247,250,252)">
+            <Tr className={styles['table-header']}>
               <Th>
-                <Flex>
-                  <Image src={nameIcon} mr={2} />
-                  <Text>Name</Text>
+                <Flex align="center">
+                  <Image src={peopleIcon} mr={2} color="black" />
+                  <Text color="black">Name</Text>
                 </Flex>
               </Th>
-              <Th align="center">
-                <EmailIcon mr={2} />
-                Email Address
+              <Th>
+                <Flex align="center">
+                  <EmailIcon mr={2} color="black" />
+                  <Text color="black">Email Address</Text>
+                </Flex>
               </Th>
               <Th>
-                <PhoneIcon mr={2} />
-                Phone Number
+                <Flex align="center">
+                  <PhoneIcon mr={2} color="black" />
+                  <Text color="black">Phone Number</Text>
+                </Flex>
               </Th>
               <Th>
                 <Flex>
                   <Image src={cardAccount} mr={2} />
-                  Role
+                  <Text color="black">Role</Text>
                 </Flex>
               </Th>
               <Th />
             </Tr>
           </Thead>
-          <Tbody>
+          <Tbody className={styles['row-text']}>
             {names.map((name, index) => (
               <Tr key="{name}">
                 <Td>{name}</Td>
@@ -109,11 +121,11 @@ const ManageStaff = () => {
                 <Td>{phones[index]}</Td>
                 <Td>
                   {roles[index] === 'Admin' ? (
-                    <Tag size="sm" variant="solid" colorScheme="blue">
+                    <Tag size="sm" variant="solid" colorScheme="blue" font="Inter" fontSize="14px">
                       Admin
                     </Tag>
                   ) : (
-                    <Tag size="sm" variant="solid" colorScheme="red">
+                    <Tag size="sm" variant="solid" colorScheme="red" font="Inter" fontSize="14px">
                       Driver
                     </Tag>
                   )}
