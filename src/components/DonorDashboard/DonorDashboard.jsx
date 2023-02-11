0eb4d3860b72f909f107e6ece820a7958540fe3e
michellelin1 <michellelin810@gmail.com>
2023-02-11 12:12:27 -0800

    add footer to donor dashboard

--- src/components/DonorDashboard/DonorDashboard.jsx
+++ src/components/DonorDashboard/DonorDashboard.jsx
@@ -1,16 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Grid, GridItem, Box } from '@chakra-ui/react';
 import TrackDonationCard from './TrackDonationCard';
 import styles from './DonorDashboard.module.css';
 
 const DonorDashboard = ({ donationId }) => {
   return (
-    <>
-      <div className={styles.track}>
-        <h1 className={styles.title}>Track your donation</h1>
-        <TrackDonationCard donationId={donationId} />
-      </div>
-    </>
+    <div className={styles.background}>
+      <Grid templateColumns="repeat(3, 1fr)" gap={10} className={styles.content}>
+        <GridItem colSpan={2}>
+          <h1 className={styles.title}>My Forms</h1>
+          <Box bg="white" w="100%" h="500" p={4} />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <h1 className={styles.title}>Pick Up</h1>
+          <Box bg="white" w="100%" h="500" p={4} />
+        </GridItem>
+        <GridItem colSpan={3}>
+          <h1 className={styles.title}>Track your donation</h1>
+          <TrackDonationCard donationId={donationId} />
+        </GridItem>
+      </Grid>
+      <footer>
+        <Grid templateColumns="repeat(9, 1fr)" className={styles.footerContent}>
+          <GridItem colSpan={4} className={styles.links}>
+            <a href="https://www.patriotsandpaws.org/our-story/">About Us</a>
+            <a href="https://www.patriotsandpaws.org/wanted/">Volunteer</a>
+            <a href="https://www.patriotsandpaws.org/asked-questions/">FAQ</a>
+            <a href="https://www.patriotsandpaws.org/donors">Donors & Supporters</a>
+          </GridItem>
+          <GridItem colSpan={1}>
+            <a href="https://www.patriotsandpaws.org/" className={styles.pnp}>
+              Patriots & Paws
+            </a>
+          </GridItem>
+        </Grid>
+      </footer>
+    </div>
   );
 };
 
--- src/components/DonorDashboard/TrackDonationCard.jsx
+++ src/components/DonorDashboard/TrackDonationCard.jsx
@@ -30,7 +30,11 @@ const TrackDonationCard = ({ donationId }) => {
   return (
     <>
       <Grid templateColumns="repeat(4, 1fr)" gap={0}>
-        <GridItem w="100%" className={stage === 1 ? styles.currStatus : styles.statusBox}>
+        <GridItem
+          w="100%"
+          className={stage === 1 ? styles.currStatus : styles.statusBox}
+          bg="white"
+        >
           <div className={styles.container}>
             <img src={stage >= 1 ? checkMark : circle} alt="check mark" />
             <div>
@@ -42,7 +46,11 @@ const TrackDonationCard = ({ donationId }) => {
           </div>
         </GridItem>
 
-        <GridItem w="100%" className={stage === 2 ? styles.currStatus : styles.statusBox}>
+        <GridItem
+          w="100%"
+          className={stage === 2 ? styles.currStatus : styles.statusBox}
+          bg="white"
+        >
           <div className={styles.container}>
             <img src={stage >= 2 ? checkMark : circle} alt="check mark" />
             <div>
@@ -54,7 +62,11 @@ const TrackDonationCard = ({ donationId }) => {
           </div>
         </GridItem>
 
-        <GridItem w="100%" className={stage === 3 ? styles.currStatus : styles.statusBox}>
+        <GridItem
+          w="100%"
+          className={stage === 3 ? styles.currStatus : styles.statusBox}
+          bg="white"
+        >
           <div className={styles.container}>
             <img src={stage >= 3 ? checkMark : circle} alt="check mark" />
             <div>
@@ -66,7 +78,11 @@ const TrackDonationCard = ({ donationId }) => {
           </div>
         </GridItem>
 
-        <GridItem w="100%" className={stage === 4 ? styles.currStatus : styles.statusBox}>
+        <GridItem
+          w="100%"
+          className={stage === 4 ? styles.currStatus : styles.statusBox}
+          bg="white"
+        >
           <div className={styles.container}>
             <img src={stage >= 4 ? checkMark : circle} alt="check mark" />
             <div>
