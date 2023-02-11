4eca024213fe956bc62961c48b59aefb60653f63
Caroline Nguyen <carolbnguyen@gmail.com>
2023-02-11 02:25:05 -0800

    Create RouteUtils file for backend calls

---
+++ src/utils/RouteUtils.js
@@ -0,0 +1,48 @@
+import { PNPBackend } from './utils';
+import AUTH_ROLES from './AuthConfig';
+
+const { DRIVER_ROLE } = AUTH_ROLES.AUTH_ROLES;
+
+const getAllRoutes = async () => {
+  const routes = await PNPBackend.get(`/routes/`);
+  return routes;
+};
+
+const getRoute = async routeId => {
+  const routes = await PNPBackend.get(`/routes/${routeId}`);
+  return routes;
+};
+
+const createRoute = async route => {
+  const { driverId, name, date } = route;
+  try {
+    await PNPBackend.post('/routes/', {
+      driverId,
+      name,
+      date,
+    });
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+const updateRoute = async route => {
+  const { routeId, driverId, name, date } = route;
+  try {
+    await PNPBackend.put(`/routes/${routeId}`, {
+      driverId,
+      name,
+      date,
+    });
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+const getDrivers = async () => {
+  const users = await PNPBackend.get('/users/');
+  const drivers = users.data.filter(user => user.role === DRIVER_ROLE);
+  return drivers;
+};
+
+export { getAllRoutes, getRoute, createRoute, updateRoute, getDrivers };
