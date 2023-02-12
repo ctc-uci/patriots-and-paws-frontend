b92b43674be6cac4c09de4a18bc483c8de87bfe7
Kristen Yee <kristecy@uci.edu>
2023-02-12 01:09:53 -0800

    fixed email formatting

--- src/components/InventoryPage/EmailModal.jsx
+++ src/components/InventoryPage/EmailModal.jsx
@@ -61,72 +61,115 @@ function EmailModal({ isOpenEmailModal, onCloseEmailModal, status, updateDonatio
       <Modal isOpen={isOpenEmailModal} onClose={onCloseEmailModal} size="full">
         <ModalOverlay />
         <ModalContent>
-          {status === 'cancel pickup' ? <ModalHeader fontSize={36}>Cancel Pickup</ModalHeader> : ''}
-          {status === 'request changes' && (
-            <ModalHeader fontSize={36}>[ACTION REQUIRED] Changes Requested</ModalHeader>
+          {status === 'cancel pickup' ? (
+            <ModalHeader m={30} fontSize={36}>
+              Cancel Pickup
+            </ModalHeader>
+          ) : (
+            ''
           )}
-          {status === 'approve' && <ModalHeader fontSize={36}>Approve Donation</ModalHeader>}
-          {status === 'scheduled' && <ModalHeader fontSize={36}>Schedule Pickup</ModalHeader>}
+          {status === 'request changes' && (
+            <ModalHeader m={30} fontSize={36}>
+              [ACTION REQUIRED] Changes Requested
+            </ModalHeader>
+          )}
+          {status === 'approve' && (
+            <ModalHeader m={30} fontSize={36}>
+              Approve Donation
+            </ModalHeader>
+          )}
+          {status === 'scheduled' && (
+            <ModalHeader m={30} fontSize={36}>
+              Schedule Pickup
+            </ModalHeader>
+          )}
           <ModalCloseButton />
           {status === 'scheduled' ? (
-            <ModalBody>
-              <Box h="400px" w="100%" bg="#EDF1F8">
-                <Box h="300px" w="95%" bg="#E2E8F0">
-                  <Text>
-                    Dear Patriots and Paws Donor, We have scheduled your donation pickup for
-                    February 8th, 2023. Please navigate to the Donation Dashboard using this link in
-                    order to accept or reschedule your pickup. If you have any questions or
-                    concerns, email patriotsandpaws@gmail.com.
+            <ModalBody mt={5}>
+              <br />
+              <Box bg="#E2E8F0" mr={79} ml={79}>
+                <br />
+                <Box p={20} mr={20} ml={20} mt={20} bg="#EDF1F8">
+                  <Text fontSize={22}>Dear Patriots and Paws Donor,</Text>
+                  <br />
+                  <Text fontSize={22}>
+                    We have scheduled your donation pickup for February 8th, 2023. Please navigate
+                    to the Donation Dashboard using this link in order to accept or reschedule your
+                    pickup. If you have any questions or concerns, email patriotsandpaws@gmail.com.
                   </Text>
-                  <Textarea bg="white" placeholder="Write message here" />
                 </Box>
+                <Box mr={20} ml={20} mt={2} mb={20} bg="#EDF1F8">
+                  <Textarea bg="white" size="lg" placeholder="Write message here" />
+                </Box>
+                <br />
               </Box>
             </ModalBody>
           ) : (
             ''
           )}
           {status === 'request changes' && (
-            <ModalBody>
-              <Box h="400px" w="100%" bg="#EDF1F8">
-                <Box h="300px" w="95%" bg="#E2E8F0">
-                  <Text>
-                    Dear Patriots and Paws Donor, We have requested changes to your donation form
-                    due to reasons listed before. We have listed the items that we don’t accept
-                    below. Please remove these items from your donation form so that we can proceed
-                    with the donation pickup. Feel free to email us at patriotsandpaws@gmail.com or
-                    call us at [pnp number] for more information or assistance.
+            <ModalBody mt={5}>
+              <br />
+              <Box bg="#E2E8F0" mr={79} ml={79}>
+                <br />
+                <Box p={20} mr={20} ml={20} mt={20} bg="#EDF1F8">
+                  <Text fontSize={22}>Dear Patriots and Paws Donor,</Text>
+                  <br />
+                  <Text fontSize={22}>
+                    We have requested changes to your donation form due to reasons listed before. We
+                    have listed the items that we don’t accept below. Please remove these items from
+                    your donation form so that we can proceed with the donation pickup. Feel free to
+                    email us at patriotsandpaws@gmail.com or call us at [pnp number] for more
+                    information or assistance.
                   </Text>
                 </Box>
-                <Textarea bg="white" placeholder="Write message here" />
+                <Box mr={20} ml={20} mt={2} mb={20} bg="#EDF1F8">
+                  <Textarea bg="white" size="lg" placeholder="Write message here" />
+                </Box>
+                <br />
               </Box>
             </ModalBody>
           )}
           {status === 'approve' && (
-            <ModalBody>
-              <Box h="400px" w="100%" bg="#EDF1F8">
-                <Box h="300px" w="95%" bg="#E2E8F0">
-                  <Text>
-                    Dear Patriots and Paws Donor, Thank you for filling out the Donation form! We
-                    have approved your donation and are working on scheduling a pickup day. Once a
-                    pickup day has been picked on our side, you will get an email to approve or
-                    reject the scheduled day. Once again, thank you for supporting our veterans!
+            <ModalBody mt={5}>
+              <br />
+              <Box bg="#E2E8F0" mr={79} ml={79}>
+                <br />
+                <Box p={20} mr={20} ml={20} mt={20} bg="#EDF1F8">
+                  <Text fontSize={22}>Dear Patriots and Paws Donor,</Text>
+                  <br />
+                  <Text fontSize={22}>
+                    Thank you for filling out the Donation form! We have approved your donation and
+                    are working on scheduling a pickup day. Once a pickup day has been picked on our
+                    side, you will get an email to approve or reject the scheduled day. Once again,
+                    thank you for supporting our veterans!
                   </Text>
                 </Box>
-                <Textarea bg="white" placeholder="Write message here" />
+                <Box mr={20} ml={20} mt={2} mb={20} bg="#EDF1F8">
+                  <Textarea bg="white" size="lg" placeholder="Write message here" />
+                </Box>
+                <br />
               </Box>
             </ModalBody>
           )}
           {status === 'cancel pickup' && (
-            <ModalBody>
-              <Box h="400px" w="100%" bg="#EDF1F8">
-                <Box h="300px" w="95%" bg="#E2E8F0">
-                  <Text>
-                    Dear Patriots and Paws Donor, Unfortunately, we have CANCELLED your pickup for
-                    this day. You can either reschedule your pickup or cancel it altogether. Please
-                    provide this information through the Donation Dashboard at this link.
+            <ModalBody mt={5}>
+              <br />
+              <Box bg="#E2E8F0" mr={79} ml={79}>
+                <br />
+                <Box p={20} mr={20} ml={20} mt={20} bg="#EDF1F8">
+                  <Text fontSize={22}>Dear Patriots and Paws Donor,</Text>
+                  <br />
+                  <Text fontSize={22}>
+                    Unfortunately, we have CANCELLED your pickup for this day. You can either
+                    reschedule your pickup or cancel it altogether. Please provide this information
+                    through the Donation Dashboard at this link.
                   </Text>
                 </Box>
-                <Textarea bg="white" placeholder="Write message here" />
+                <Box mr={20} ml={20} mt={2} mb={20} bg="#EDF1F8">
+                  <Textarea bg="white" size="lg" placeholder="Write message here" />
+                </Box>
+                <br />
               </Box>
             </ModalBody>
           )}
@@ -144,17 +187,16 @@ function EmailModal({ isOpenEmailModal, onCloseEmailModal, status, updateDonatio
             ) : (
               ''
             )}
-            {status === 'request changes' ||
-              (status === 'scheduled' && (
-                <Button
-                  colorScheme="blue"
-                  onClick={() => {
-                    updateDonationStatus('changes requested');
-                  }}
-                >
-                  Send Email
-                </Button>
-              ))}
+            {(status === 'request changes' || status === 'scheduled') && (
+              <Button
+                colorScheme="blue"
+                onClick={() => {
+                  updateDonationStatus('changes requested');
+                }}
+              >
+                Send Email
+              </Button>
+            )}
             {status === 'approve' && (
               <Button
                 colorScheme="green"
