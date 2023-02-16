2c2d121d42f7bae23648c65e818db5b7fb7d0532
Rachel Villamor <ravillamor23@gmail.com>
2023-02-16 14:48:11 -0800

    Started implementing styling

--- src/components/EditItemsList/EditItemsList.jsx
+++ src/components/EditItemsList/EditItemsList.jsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import { Wrap, Input, Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react';
+import React, { useState, useEffect, useRef } from 'react';
+import { Wrap, Input, Box, Button, Flex } from '@chakra-ui/react';
 import ItemCard from '../ItemCard/ItemCard';
 
-const EditItemsList = () => {
-  const title = 'neither accepted nor non accepted cards. nothing!';
+const EditItemsList = ({ items, setItems, isAccepted = false }) => {
+  const ref = useRef();
+
+  const handleAdd = () => {
+    // add validation to make sure current value isn't already in items
+    // if it is already in items, set an error message (see chakra form control)
+    setItems(prev => [...prev, { name: ref.current.value, accepted: isAccepted }]);
+  };
+  // maxW prop
   return (
-    <>
-      <Card>
-        <CardHeader>{title}</CardHeader>
-        <CardBody>
-          <Wrap>
-            <ItemCard />
-            <ItemCard />
-          </Wrap>
-        </CardBody>
-        <CardFooter>
-          <Input />
-        </CardFooter>
-      </Card>
-    </>
+    <Box bg="gray.50" width="50%" height="100%" mx="3vh" overflow="hidden">
+      <Wrap height="100%" minHeight="50vh" my="3%">
+        {items
+          .filter(({ accepted }) => accepted === isAccepted)
+          .map(({ name }) => (
+            <ItemCard key={name} name={name} />
+          ))}
+      </Wrap>
+      <Flex>
+        <Input ref={ref} />
+        <Button onClick={handleAdd}> Add Item to List</Button>
+      </Flex>
+    </Box>
   );
 };
 
