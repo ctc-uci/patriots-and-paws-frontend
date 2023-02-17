b3ad69f774418286604a5cbb484d8cc624b6eb69
NwinNwin <ndnguyen2003@gmail.com>
2023-02-16 16:26:34 -0800

    working with backend
    working with backend

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Flex, Heading, Stack, Text, Button } from '@chakra-ui/react';
-import { getUserFromDB, getCurrentUser, auth } from '../../utils/AuthUtils';
+import { Flex, Heading, Stack, Text } from '@chakra-ui/react';
+import Logout from '../../components/Logout/Logout';
+import { getUserFromDB } from '../../utils/AuthUtils';
 
 const Dashboard = () => {
   const [user, setUser] = useState({});
 
   useEffect(() => {
     const fetchUserFromDB = async () => {
-      const { uid } = await getCurrentUser(auth);
-      const userFromDB = await getUserFromDB(uid);
+      const userFromDB = await getUserFromDB();
       setUser(userFromDB);
     };
     fetchUserFromDB();
@@ -20,9 +19,7 @@ const Dashboard = () => {
       <Stack align="center">
         <Heading>DASHBOARD</Heading>
         <Text>Hello, {user.firstName}!</Text>
-        <Button colorScheme="blue">
-          <Link to={`/users/${user.id}`}>User Profile</Link>
-        </Button>
+        <Logout />
       </Stack>
     </Flex>
   );
