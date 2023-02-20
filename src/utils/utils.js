bcd1c9c92f9344828651e116bca27850b2e197d1
Alan Chang <alan@alanchang.xyz>
2023-02-20 02:00:46 -0800

    Separate dev/prod backend endpoints

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,18 +1,12 @@
 import axios from 'axios';
 import { renderEmail } from 'react-html-email';
 
-let baseURL = '';
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-  // dev code
-  baseURL = `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}`;
-} else {
-  // production code
-  baseURL = `${process.env.REACT_APP_PROD_API_URL}`;
-}
-
 // See auth_utils for AuthInterceptor
 const PNPBackend = axios.create({
-  baseURL,
+  baseURL:
+    !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+      ? process.env.REACT_APP_BACKEND_HOST
+      : process.env.REACT_APP_BACKEND_HOST_PROD,
   withCredentials: true,
 });
 
