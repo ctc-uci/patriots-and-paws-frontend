acb2384a5c5fc76d82031b7a0f252d2a0179282d
jrock-3 <justin.liao3@gmail.com>
2023-03-02 13:17:21 -0800

    Fix styling, add notes section, small refactoring

--- src/components/RoutePDF/RoutePDF.jsx
+++ src/components/RoutePDF/RoutePDF.jsx
@@ -11,13 +11,14 @@ import {
   Image,
   Font,
 } from '@react-pdf/renderer';
-import { getRouteDonations, getDriver } from '../../utils/routesUtils';
+import { getRouteDonations, formatDate, getDriverName } from '../../utils/routesUtils';
 import { formatPhone } from '../../utils/utils';
 import deliveryIcon from '../../assets/delivery.png';
 import itemIcon from '../../assets/item.png';
 import emailIcon from '../../assets/email.png';
 import phoneIcon from '../../assets/phone.png';
 import driverIcon from '../../assets/driver.png';
+import commentIcon from '../../assets/comment.png';
 // import timeIcon from '../../assets/time.png';
 import InterRegular from '../../assets/Inter/Inter-Regular.ttf';
 import InterBold from '../../assets/Inter/Inter-Bold.ttf';
@@ -37,15 +38,11 @@ const RoutePDF = ({ routeID }) => {
 
   useEffect(() => {
     const helper = async () => {
-      const info = await getRouteDonations(routeID);
+      const { donations: donationsList, date, driverId } = await getRouteDonations(routeID);
+      setDonations(donationsList);
+      setDateStr(formatDate(new Date(date)));
 
-      setDonations(info.donations);
-
-      const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-      const date = new Date(info.date);
-      setDateStr(date.toLocaleDateString('en-US', options));
-
-      const driverName = await getDriver(info.driverId);
+      const driverName = await getDriverName(driverId);
       setDriver(driverName);
     };
     helper();
@@ -57,6 +54,7 @@ const RoutePDF = ({ routeID }) => {
       margin: 30,
       fontSize: 13,
       fontFamily: 'Inter',
+      paddingBottom: 30,
     },
     section: {
       margin: 10,
@@ -105,9 +103,13 @@ const RoutePDF = ({ routeID }) => {
       flexDirection: 'row',
     },
     furnitureList: {
+      marginBottom: 20,
+    },
+    donation: {
       marginBottom: 30,
     },
   });
+
   const donationsList = donations.map(
     ({
       id,
@@ -120,8 +122,9 @@ const RoutePDF = ({ routeID }) => {
       email,
       phoneNum,
       furniture,
+      notes,
     }) => (
-      <View key={id}>
+      <View key={id} style={styles.donation}>
         <Svg height="10" width="520">
           <Line x1="0" y1="0" x2="520" y2="0" strokeWidth={2} stroke="#939393" />
         </Svg>
@@ -173,6 +176,16 @@ const RoutePDF = ({ routeID }) => {
             </Text>
           ))}
         </View>
+
+        {notes && (
+          <View>
+            <View style={styles.iconTitleContainer}>
+              <Image src={commentIcon} style={styles.icon} />
+              <Text style={styles.subTitle}>Notes:</Text>
+            </View>
+            <Text style={styles.indent}>{notes}</Text>
+          </View>
+        )}
       </View>
     ),
   );
@@ -189,7 +202,9 @@ const RoutePDF = ({ routeID }) => {
     </Document>
   );
 };
+
 RoutePDF.propTypes = {
   routeID: PropTypes.number.isRequired,
 };
+
 export default RoutePDF;
--- src/utils/routesUtils.js
+++ src/utils/routesUtils.js
@@ -1,14 +1,24 @@
 import { PNPBackend } from './utils';
 
 const getRouteDonations = async routeId => {
-  const donations = await PNPBackend.get(`/routes/${routeId}`);
-  return donations.data[0];
+  const { data } = await PNPBackend.get(`/routes/${routeId}`);
+  return data[0];
 };
 
-const getDriver = async driverId => {
-  const res = await PNPBackend.get(`/users/${driverId}`);
-  const user = res.data[0];
+const getDriverName = async driverId => {
+  const { data } = await PNPBackend.get(`/users/${driverId}`);
+  const user = data[0];
   return `${user.firstName} ${user.lastName}`;
 };
 
-export { getRouteDonations, getDriver };
+const formatDate = date =>
+  date.toLocaleDateString('en-US', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
+const A4_WIDTH = 595.28; // can use instead of "A4" for page size to get one long page
+
+export { getRouteDonations, getDriverName, formatDate, A4_WIDTH };
