ad5dfd64199ec21d9e6699044ab290ee9f193e76
NwinNwin <ndnguyen2003@gmail.com>
2023-04-11 12:31:32 -0700

    working on driverdashboard

--- src/pages/Dashboard/DriverDashboard.jsx
+++ src/pages/Dashboard/DriverDashboard.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Flex } from '@chakra-ui/react';
+import { Flex, Box } from '@chakra-ui/react';
 import RouteCalendar from '../../components/RouteCalendar/RouteCalendar';
 import TodayRoute from '../../components/TodayRoute/TodayRoute';
 
 const DriverDashboard = () => {
   return (
     <Flex direction="row">
-      <TodayRoute mr={5} />
-      <RouteCalendar />
+      <Box width="40vw" bg="#EDF1F8">
+        <TodayRoute />
+      </Box>
+      <Box height="90vh" width="60vw" padding="40px">
+        <RouteCalendar />
+      </Box>
     </Flex>
   );
 };
