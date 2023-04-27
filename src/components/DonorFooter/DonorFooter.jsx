3bc2d6abf1822d4c5b78929ca75f0c384a5d6d16
michellelin1 <michellelin810@gmail.com>
2023-04-26 17:53:29 -0700

    mobile view donation footer

--- src/components/DonorFooter/DonorFooter.jsx
+++ src/components/DonorFooter/DonorFooter.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Flex, Link, Text } from '@chakra-ui/react';
+import { Flex, Link, Text, LinkBox, LinkOverlay, Image, Box } from '@chakra-ui/react';
+import pnpLogo from './PNPlogo.png';
 
 const DonorFooter = () => {
   return (
@@ -14,24 +15,50 @@ const DonorFooter = () => {
       position="sticky"
       top="100vh"
       justifyContent="space-between"
+      direction={{ md: 'row', base: 'column' }}
     >
-      <Flex justifyContent="space-between" gap="10px">
-        <Link href="https://www.patriotsandpaws.org/our-story/" isExternal>
-          <Text as="u">About Us</Text>
-        </Link>
-        <Link href="https://www.patriotsandpaws.org/wanted/" isExternal>
-          <Text as="u">Volunteer</Text>
-        </Link>
-        <Link href="https://www.patriotsandpaws.org/asked-questions/" isExternal>
-          <Text as="u">FAQ</Text>
-        </Link>
-        <Link href="https://www.patriotsandpaws.org/donors" isExternal>
-          <Text as="u">Donors & Supporters</Text>
+      <Flex direction={{ md: 'row', base: 'column' }} align="center">
+        <LinkBox mr={{ md: 10 }}>
+          <LinkOverlay href="https://www.patriotsandpaws.org/" isExternal>
+            <Image
+              boxSize="3rem"
+              src={pnpLogo}
+              alt="Patriots and Paws logo, redirects to main page"
+            />
+          </LinkOverlay>
+        </LinkBox>
+        <Link
+          href="https://www.patriotsandpaws.org/"
+          fontSize="18px"
+          visibility={{ md: 'visible', base: 'hidden' }}
+        >
+          Patriots & Paws
         </Link>
       </Flex>
-      <Link href="https://www.patriotsandpaws.org/" color="red.500" fontSize="20px">
-        Patriots & Paws
-      </Link>
+      {/* <Flex justifyContent="space-between" gap="10px"> */}
+      <Box display={{ md: 'flex' }} gap={{ md: '10px' }} mt={{ md: '10px' }}>
+        <Box>
+          <Link href="https://www.patriotsandpaws.org/our-story/" isExternal>
+            <Text as="u">About Us</Text>
+          </Link>
+        </Box>
+        <Box mt={{ md: 0, base: 3 }}>
+          <Link href="https://www.patriotsandpaws.org/wanted/" isExternal>
+            <Text as="u">Volunteer</Text>
+          </Link>
+        </Box>
+        <Box mt={{ md: 0, base: 3 }}>
+          <Link href="https://www.patriotsandpaws.org/donors" isExternal>
+            <Text as="u">Donors & Supporters</Text>
+          </Link>
+        </Box>
+        <Box mt={{ md: 0, base: 3 }}>
+          <Link href="https://www.patriotsandpaws.org/asked-questions/" isExternal>
+            <Text as="u">FAQ</Text>
+          </Link>
+        </Box>
+      </Box>
+      {/* </Flex> */}
     </Flex>
   );
 };
